5b13632363e9ba376de1cf0841a9d449753fad76
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-09-07 17:58:01 +0530

    initial commit

---
+++ src/router/Routes.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Shop from "../pages/Shop";
+import Blog from "../pages/Blog";
+import Contact from "../pages/Contact";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
